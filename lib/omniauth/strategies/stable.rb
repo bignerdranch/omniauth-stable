20c16f5d3c54a345be0a19db41cbd4567ecacc00
Stafford Brooke <staffordbrooke@gmail.com>
2013-08-01 17:09:57 -0400

    Remove trailing slashes

--- lib/omniauth/strategies/stable.rb
+++ lib/omniauth/strategies/stable.rb
@@ -3,13 +3,13 @@ require 'omniauth/strategies/oauth2'
 module OmniAuth
   module Strategies
     class Stable < OmniAuth::Strategies::OAuth2
-      API_ENDPOINT = "https://stable.bignerdranch.com/"
+      API_ENDPOINT = "https://stable.bignerdranch.com"
       option :name, "stable"
 
       # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
       option :client_options, {
-        site:           "https://stable.bignerdranch.com/",
+        site:           "https://stable.bignerdranch.com",
         authorize_url:  "oauth/authorize",
         token_url:      "oauth/token"
       }
@@ -21,7 +21,7 @@ module OmniAuth
 
       extra do
         {
-          raw_info: {}
+          "raw_info" => {}
         }
       end
     end
